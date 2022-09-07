a3d7a7af98622543e4c6ffa912a70e7a290431dc
Dezhi <55412122+DravenCat@users.noreply.github.com>
2022-09-07 17:06:25 -0400

    complete link card feature

---
+++ src/component/linkCard/linkCard.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import {
+  ChakraProvider,
+  LinkBox,
+  Heading,
+  Text,
+  LinkOverlay,
+  Box,
+} from "@chakra-ui/react";
+import { transforms } from "../../constants/motion";
+
+export default class LinkCard extends Component {
+  render() {
+    return (
+      <ChakraProvider>
+        <Box p={6}>
+          <LinkBox
+            boxSize="250"
+            borderRadius="lg"
+            boxShadow="lg"
+            display="flex"
+            flexDirection="column"
+            color="white"
+            background="#5490db"
+            fontSize={[`lg`, null, `md`, `1.125rem`, `1.3125rem`]}
+            sx={{ textShadow: `0 1px 2px rgba(0, 0, 0, 0.5)` }}
+            transition={transforms.beforeHover.transition}
+            transform={transforms.beforeHover.transform}
+            _hover={{
+              transform: transforms.onHover.transform,
+              boxShadow: transforms.onHover.boxShadow,
+            }}
+          >
+            <Heading size="lg" margin="5px" mb="10">
+              <LinkOverlay href="#" mb="1em">
+                {this.props.name}
+              </LinkOverlay>
+            </Heading>
+            <Text fontSize="lg" padding="5px" ml="5px" mr="5px">
+              {this.props.description}
+            </Text>
+          </LinkBox>
+        </Box>
+      </ChakraProvider>
+    );
+  }
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,27 @@
 import "./home.css";
 import Introduction from "../../component/introduction/introduction";
 import HomePageHeader from "../../component/homePageHeader/homePageHeader";
+import LinkCard from "../../component/linkCard/linkCard";
 
 function Home() {
   return (
     <div className="home_page">
       <HomePageHeader />
       <Introduction />
-      <div className="navigation-button"></div>
+      <div className="navigation-button">
+        <LinkCard
+          name="My tech stack"
+          description="Click to review my technology experience!"
+        />
+        <LinkCard
+          name="My project"
+          description="Click to review my past project! Many of them are developed by React, Node.js, Java and C"
+        />
+        <LinkCard
+          name="Contact me"
+          description="Click to review my contact information!"
+        />
+      </div>
     </div>
   );
 }
