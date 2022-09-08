383532cd76929b00230868be05121c09ff0f4240
Dezhi <55412122+DravenCat@users.noreply.github.com>
2022-09-08 06:34:07 -0400

    hash

--- src/component/navigation/navigation.js
+++ src/component/navigation/navigation.js
@@ -7,14 +7,14 @@ import HomeIcon from "@rsuite/icons/legacy/Home";
 function Navigation() {
   return (
     <Navbar fixed="top">
-      <Navbar.Brand href="/#/">Geralt's personal website</Navbar.Brand>
+      <Navbar.Brand href="#/">Geralt's personal website</Navbar.Brand>
       <Nav>
-        <Nav.Item href="/#/" icon={<HomeIcon />}>
+        <Nav.Item href="#/" icon={<HomeIcon />}>
           Home
         </Nav.Item>
-        <Nav.Item href="/#/techStack">TechStack</Nav.Item>
-        <Nav.Item href="/#/project">Project</Nav.Item>
-        <Nav.Item href="/#/contact">Contact</Nav.Item>
+        <Nav.Item href="#/techStack">TechStack</Nav.Item>
+        <Nav.Item href="#/project">Project</Nav.Item>
+        <Nav.Item href="#/contact">Contact</Nav.Item>
       </Nav>
     </Navbar>
   );
